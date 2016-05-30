82cc4f787803a4a90e1b4c3caea880a4c5b2e125
luchanso <oleg@loutchansky.com>
2016-05-30 13:11:58 +0300

    Finish game

--- public/app.js
+++ public/app.js
@@ -1,43 +1,75 @@
-var game = new Phaser.Game(1900, 1060, Phaser.AUTO);
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
 var test = {
   players: [],
+  pnames: [],
 
   preload: function() {
-    this.game.stage.backgroundColor = 0x000000 /*0x65A2EE*/ ;
+    this.game.stage.backgroundColor = 0x000000;
     this.game.scale.pageAlignHorizontally = true;
     this.game.scale.pageAlignVertically = true;
 
     this.game.load.image('cude', 'square.png');
+    this.game.load.spritesheet('kaboom', 'explosion.png', 128, 128);
     this.game.load.json('userData', 'getUserData');
 
     this.game.load.onLoadComplete.add(this.updateUsers, this);
   },
 
   create: function() {
-    this.timer = this.game.time.create(false);
-    this.timer.loop(60 * 1000, this.updateUsers, this);
-    this.timer.start();
+    this.explosions = game.add.group();
+    this.explosions.createMultiple(30, 'kaboom');
+    this.explosions.forEach(this.setupInvader, this);
 
-    this.checkUsers();
+    this.timer = this.game.time.create(false);
+    this.timer.loop(30 * 1000, this.updateUsers, this);
+    this.timer.start();
+  },
+
+  boom: function(x, y) {
+    var explosion = this.explosions.getFirstExists(false);
+    explosion.reset(x, y);
+    explosion.play('kaboom', 30, false, true);
+  },
+
+  setupInvader: function(invader) {
+    invader.anchor.x = 0.5;
+    invader.anchor.y = 0.5;
+    invader.animations.add('kaboom');
   },
 
   update: function() {
-    this.game.physics.arcade.collide(this.players, this.players);
-  },
-
-  checkUsers: function() {
-
+    this.game.physics.arcade.collide(this.players, this.players, function(obj1, obj2) {
+      var pt = Phaser.Point.centroid([new Phaser.Point(obj1.x, obj2.y), new Phaser.Point(obj1.x, obj2.y)]);
+      this.boom(pt.x, pt.y);
+    }, null, this);
   },
 
   updateUsers: function() {
     this.game.load.json('userData', 'getUserData', true);
 
-    this.players.forEach(function(pl) {
-      pl.destroy();
-    });
+    var summary = this.game.cache.getJSON('userData').chatters.viewers;
+    summary = summary.concat(this.game.cache.getJSON('userData').chatters.moderators);
 
-    this.players = this.game.cache.getJSON('userData').chatters.viewers.map(function(p) {
+    this.pnames = this.pnames.filter(function(pname) {
+      if (summary.indexOf(pname) === -1) {
+        return;
+      } else {
+        return pname;
+      }
+    }, this);
+
+    summary = summary.filter(function(pname) {
+      if (this.pnames.indexOf(pname) === -1) {
+        return pname;
+      } else {
+        return;
+      }
+    }, this);
+
+    this.pnames = this.pnames.concat(summary);
+
+    var newPlayers = summary.map(function(pname) {
       var sprite = this.game.add.sprite(0, 0, 'cude');
       sprite.width = 25;
       sprite.height = 25;
@@ -53,8 +85,8 @@ var test = {
         fill: "#fff"
       }
 
-      var text = this.game.add.text(sprite.width / 2, sprite.height + 5, p, style);
-      text.anchor.set(0.5, 0);      
+      var text = this.game.add.text(sprite.width / 2, sprite.height + 5, pname, style);
+      text.anchor.set(0.5, 0);
 
       result.addChild(sprite);
       result.addChild(text);
@@ -73,7 +105,20 @@ var test = {
       result.body.width = sprite.width;
       result.body.height = sprite.height;
 
+      result.pname = pname;
+
       return result;
+    }, this);
+
+    this.players = newPlayers.concat(this.players);
+
+    this.players.filter(function(pl) {
+      if (this.pnames.indexOf(pl.pname) === -1) {
+        pl.destroy();
+        return;
+      } else {
+        return pl;
+      }
     }, this);
   }
 }
